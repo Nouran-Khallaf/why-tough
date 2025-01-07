b8300bab4be29ac46eb12c014d5e8706688f8770
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2025-01-07 03:48:26 +0000

    Update model.py

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ from transformers import (
     DataCollatorWithPadding,
     EarlyStoppingCallback
 )
+from typology_mapping import macro_to_original  # Import the mapping
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 # Define a Custom Trainer to Incorporate Class Weights
@@ -62,37 +63,6 @@ def prepare_datasets(dataset_path, sep, valsplit):
     data['Typology'] = data['Strategy'].str.replace("\xa0", "").str.replace(" ", "")
 
     # Map Typologies to Macro-Categories
-    macro_to_original = {
-        'Transcript': ['Transcript'],
-        'Substitution': [
-            'Synonymy', 'Anaph', 'SemStereo', 'SemStere', 'SemHype', 'Hypernyms', 'Hyponyms', 'Acronyms',
-            'SemHypeÊ', 'SemStereÊ', 'SemHypo','SemHype\xa0','SemStere\xa0'
-        ],
-        'Grammatical Adjustments': [
-            'GraPron', 'GraTens', 'GraSim', 'GraPass', 'Negations', 'Pronouns', 'PassiveVoice', 'GraPassÊ','GraPass\xa0'
-        ],
-        'Explanation': [
-            'ExplWor', 'WordExpl', 'ExplCont', 'ExplExpr', 'ExpExp', 'Tropes', 'Schemes', 'Deixis', 'HidGram',
-            'HidCont', 'WorExp', 'HidGra', 'HidCon', 'PraCont', 'PraExp', 'PraExpÊ', 'PrAcron', 'PraProp'
-        ],
-        'Syntactic Changes': [
-            'SynChange', 'Clause2Word', 'WordsOrder', 'GroupOrder', 'LinearOrderSen', 'LinearOrderCla',
-            'SynW2G', 'SynG2W', 'SynC2W', 'SynG2T', 'SynW2C', 'SynW2S', 'SynC2S', 'SynG2C', 'SynG2WÊ',
-            'SynW2SÊ', 'SynSem', 'SynG2W\xa0', 'SynW2S\xa0'
-        ],
-        'Transposition': ['TranspNoun', 'TraNou', 'TraVer', 'Trans'],
-        'Modulation': [
-            'ModInfo', 'ModInf', 'ClauseOrder', 'GroupOrder', 'WordOrder', 'ModClau', 'ModWord', 'ModGrou'
-        ],
-        'Omission': [
-            'OmiSen', 'OmiWor', 'OmiClau', 'OmiRhet', 'OmiComp', 'OmiSubj', 'OmiSent', 'OmiVer', 'OmiRhe'
-        ],
-        'Simplification': [
-            'SinGram', 'SinSem', 'SimGram', 'SinPrag', 'SimplifiedSyntax', 'SimplifiedTenses', 'S-V-O Structures'
-        ],
-        'Illocutionary Change': ['IllCh']
-    }
-
     original_to_macro = {typology: macro for macro, typologies in macro_to_original.items() for typology in typologies}
     data['Typology'] = data['Strategy'].replace(original_to_macro)
 
