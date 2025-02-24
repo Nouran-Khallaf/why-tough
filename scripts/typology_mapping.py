88319c349fe7126ca6fae3ed44aa4eb726eaca4b
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2025-02-24 12:50:43 +0000

    Update typology_mapping.py

--- scripts/typology_mapping.py
+++ scripts/typology_mapping.py
@@ -5,13 +5,14 @@ macro_to_original = {
     ],
     'Synonymy': [
         'PraSyn', 'SemSyn', 'GraSyn', 'PraProp', 'PraCont', 'PrAcron', 'SemStere', 'SemHype', 'SemHypo',
-        'GraPron', 'GraTens', 'GraPass', 'GraNega'
+        'GraPron', 'GraTens', 'GraPass', 'GraNega',
     ],
     'Explanation': [
         'WorExp', 'ExpExp', 'TroExp', 'SchExp', 'DeiExp', 'HidGra', 'HidCon'
     ],
     'Syntactic Changes': [
-        'SynW2G', 'SynW2C', 'SynW2S', 'SynG2W', 'SynG2C', 'SynG2S', 'SynC2W', 'SynC2G', 'SynC2S', 'SynS2W', 'SynS2G', 'SynS2C'
+        'SynW2G', 'SynW2C', 'SynW2S', 'SynG2W', 'SynG2C', 'SynG2S', 'SynC2W', 'SynC2G', 'SynC2S', 'SynS2W', 'SynS2G', 'SynS2C',
+        'SynG2T'
     ],
     'Transposition': [
         'TranNou', 'TraVer', 'TrAdje', 'TrAdve'
@@ -20,7 +21,7 @@ macro_to_original = {
         'ModInf', 'ModLin', 'ModClau', 'ModWord', 'ModGrou'
     ],
     'Omission': [
-        'OmiEle', 'OmiRhe', 'OmiDia', 'OmiSubj', 'OmiVerb', 'OmiComp', 'OmiSent'
+        'OmiEle', 'OmiRhe', 'OmiDia', 'OmiSubj', 'OmiVerb', 'OmiComp', 'OmiSent','OmiVer'
     ],
     'Compression': [
         'GraSim', 'SemSim'
